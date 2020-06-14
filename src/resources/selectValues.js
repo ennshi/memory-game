6727306dee28f7c48a3296c767af38db2ae4615a
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-06-15 00:12:22 +0200

    selectValues list added

---
+++ src/resources/selectValues.js
@@ -0,0 +1,17 @@
+const THEMES = {
+    animals: 'animals',
+    landmarks: 'landmarks',
+    leaves: 'leaves'
+};
+const SUITS = {
+    '5': '5 suits',
+    '10': '10 suits',
+    '15': '15 suits'
+};
+const CARDS = {
+    '20': '20 cards',
+    '30': '30 cards',
+    '40': '40 cards'
+};
+
+export {THEMES, CARDS, SUITS};
