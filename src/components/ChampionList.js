af230448904acf18f25dd33d6ba4296f78ccb4a5
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-06-14 18:55:43 +0200

    Components Initialized

---
+++ src/components/ChampionList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ChampionList extends React.Component {
+    render() {
+        return (
+            <div>
+               ChampionList
+            </div>
+        );
+    }
+}
+
+export default ChampionList;
---
+++ src/components/SettingsForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SettingsForm extends React.Component {
+    render() {
+        return (
+            <div>
+                Settings Form
+            </div>
+        );
+    }
+}
+
+export default SettingsForm;
