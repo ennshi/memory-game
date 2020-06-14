c950337a9c0e2a53246ce646ab7227e0bc11fb21
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-06-14 18:44:13 +0200

    Actions added

---
+++ src/actions/types.js
@@ -0,0 +1,15 @@
+// timer actions
+export const UPDATE_TIMER = 'UPDATE_TIMER';
+export const RESET_TIMER = 'RESET_TIMER';
+export const STOP_TIMER = 'STOP_TIMER';
+
+// board actions
+export const INITIALIZE_BOARD = 'INITIALIZE_BOARD';
+export const FLIP_CARD = 'FLIP_CARD';
+export const MATCHED_CARDS = 'MATCHED_CARDS';
+
+// champions action
+export const GET_CHAMPIONS = 'GET_CHAMPIONS';
+export const SHOW_CHAMPLIST = 'SHOW_CHAMPLIST';
+export const CLEAR_CHAMPLIST = 'CLEAR_CHAMPLIST';
+export const ADD_CHAMPION = 'ADD_CHAMPION';
