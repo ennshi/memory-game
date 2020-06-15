696577c6149dd3b213ee7820d0b0c719c8d016bf
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-06-15 23:39:29 +0200

    Flipcard action added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
-import {INITIALIZE_BOARD} from "./types";
+import {FLIP_CARD, INITIALIZE_BOARD} from "./types";
 
 export const initializeBoard = (formValues) => {
     return {
         type: INITIALIZE_BOARD,
         payload: formValues
     }
+};
+
+export const flipCard = (id) => async (dispatch) => {
+    await setTimeout(() => {
+        dispatch({
+            type: FLIP_CARD,
+            payload: id
+        });
+    }, 400);
 };
