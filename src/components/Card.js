20cc2cc095acfa36dbb7f57807f523d0e62c0537
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-06-15 15:28:42 +0200

    Card rendered

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,44 @@
 import React from "react";
+import {connect} from "react-redux";
+
+const classNames = require("classnames");
 
 class Card extends React.Component {
+    onClick = () => {
+
+    };
     render() {
-        return (
-            <div>
-                Card
+        const card = this.props.board[this.props.id];
+        const cardClass = classNames({
+            'card-inner': true,
+            'clicked': card.opened,
+            'not-clicked': !card.opened,
+            'matched': card.matched
+        });
+        return(
+            <div className="card">
+                <div className={cardClass} onClick={this.onClick}>
+                    <div className={card.matched ? "card-front matched" : "card-front"}>
+                    </div>
+                    <div className="card-back">
+                        <Image theme={this.props.theme} suit={card.suit}/>
+                    </div>
+                </div>
             </div>
         );
     }
 }
+const Image = (props) => {
+    return (
+        <img src={`/img/${props.theme}/${props.suit}.png`} alt={`${props.theme} ${props.suit}`}/>
+    );
+};
+const mapStateToProps = (state, ownParams) => {
+    return {
+        board: state.board.board,
+        id: ownParams.id,
+        theme: state.board.theme
+    }
+};
 
-export default Card;
+export default connect(mapStateToProps)(Card);
