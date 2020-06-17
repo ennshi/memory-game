11c13d4d114101ce24564989f3b2eea0800cffee
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-06-17 22:31:44 +0200

    ChampionListForm rendered

--- src/components/ChampionListForm.js
+++ src/components/ChampionListForm.js
@@ -1,11 +1,42 @@
 import React from "react";
+import {Field, reduxForm, formValues} from "redux-form";
+import {connect} from "react-redux";
+import {CARDS, SUITS} from "../resources/selectValues";
 
 class ChampionListForm extends React.Component {
+    renderSelectOptions = (list) => {
+        const selectOptions = [];
+        for(const option in list) {
+            selectOptions.push(
+                <option key={option} value={option}>{list[option]}</option>
+            );
+        }
+        return selectOptions;
+    };
     render() {
         return (
-            <form></form>
+            <form onChange={this.props.onChange}>
+                <Field name="cards" component="select">
+                    {this.renderSelectOptions(CARDS)}
+                </Field>
+                <Field name="suits" component="select">
+                    {this.renderSelectOptions(SUITS)}
+                </Field>
+            </form>
         );
     }
 }
-
-export default ChampionListForm;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        initialValues: {
+            cards: state.board.cards,
+            suits: state.board.suits
+        },
+        onChange: ownProps.onChange
+    }
+};
+const wrappedForm = reduxForm({
+    form: 'ChampionsForm',
+    enableReinitialize : true
+})(ChampionListForm);
+export default connect(mapStateToProps)(wrappedForm);
