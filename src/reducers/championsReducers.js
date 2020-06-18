1683da1477553c8aeb0b48c2f0a429dc161c83c9
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-06-19 00:31:14 +0200

    ChampionsReducers added

---
+++ src/reducers/championsReducers.js
@@ -0,0 +1,21 @@
+import {GAME_ENDED, TOGGLE_CHAMPLIST} from "../actions/types";
+
+const INIT_VALUE = {
+    showChampionList: false
+};
+export default (state=INIT_VALUE, action) => {
+    switch (action.type) {
+        case TOGGLE_CHAMPLIST:
+            return {
+                ...state,
+                showChampionList: !state.showChampionList
+            };
+        case GAME_ENDED:
+            return {
+                ...state,
+                showChampionList: true
+            };
+        default:
+            return state;
+    }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux";
 import { reducer as formReducer } from "redux-form";
 import boardReducers from "./boardReducers";
 import timerReducers from "./timerReducers";
+import championsReducers from "./championsReducers";
 
 export default combineReducers({
     board: boardReducers,
     timer: timerReducers,
+    champions: championsReducers,
     form: formReducer
 });
