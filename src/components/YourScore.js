98a266f71d62db7684a241339fd96314c04c4c86
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-06-20 01:48:10 +0200

    YourScore style added

--- src/components/YourScore.js
+++ src/components/YourScore.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import {exitCurrentGame, toggleChampionList} from "../actions";
+import "../css/YourScore.css";
 
 class YourScore extends React.Component {
     onClick = () => {
