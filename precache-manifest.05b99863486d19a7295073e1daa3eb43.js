61b4fed50d6337ec04830a7279f6e1067253707f
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-06-20 23:01:23 +0200

    Updates

---
+++ precache-manifest.05b99863486d19a7295073e1daa3eb43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37689c6d92c6b2f0c86c8fac492f5508",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "a4dd83690824705096b3",
+    "url": "/memory-game/static/css/main.c2a91548.chunk.css"
+  },
+  {
+    "revision": "b88b1ed95034e7e1625c",
+    "url": "/memory-game/static/js/2.11692ebe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/memory-game/static/js/2.11692ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4dd83690824705096b3",
+    "url": "/memory-game/static/js/main.23091d33.chunk.js"
+  },
+  {
+    "revision": "cc5a158bb65db08dab34",
+    "url": "/memory-game/static/js/runtime-main.49b259c2.js"
+  }
+]);
